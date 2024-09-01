55227131366273a2ccad207a9611e6bb75b7a54e
ZaaakW <98629150+ZaaakW@users.noreply.github.com>
2024-09-02 02:10:12 +0700

    Update __init__.py

--- button_paginator/__init__.py
+++ button_paginator/__init__.py
@@ -1,3 +1,14 @@
+"""
+discord pagination
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+A python library create discord paginator to make you easy.
+
+:copyright: (c) 2024 @zaaakw
+:license: MIT
+
+"""
+
 from .paginate import Paginator, PaginatorButton
 from .types import Emoji, Page, ButtonConfig
 
