bc1db470116146a7710d2705b1ea41ea03e0fa62
ZaaakW <98629150+ZaaakW@users.noreply.github.com>
2024-09-02 02:16:55 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,10 @@ desc = (here / "README.md").read_text(encoding="utf-8")
 setup(
     name="button_paginator",
     version="1.0.0",
-    description="A module which allows easy implementation of button pagination",
+    description="A python library create a discord.py paginator",
     long_description=desc,
-    long_description_content_type="text/markdown",
+    long_description_content_type="text/markdown",,
+    license = "MIT",
     author="ZaaakW",
     url="https://github.com/ZaaakW/button_paginator",
     python_requires=">=3.6",
